b7f5bb1c111ba0648036d99dc4155c00703d4c1a
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-05-12 07:45:12 +0530

    initial setup

---
+++ AtmSimulation/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Welcome to World Bank ATM\n");
+Console.WriteLine("Please Select Your Account Type: ");
+Console.WriteLine("1. International");
+Console.WriteLine("2. Domestic");
+
+int accountType, operationType, accountPin, withdrawAmount;
+double currentBalance = 19870.79;
+
+if(!int.TryParse(Console.ReadLine(), out accountType) || (accountType != 1 && accountType != 2))
+{
+    Console.WriteLine("Transaction Canceld. Please Select a valid option");
+    return;
+}
+
+Console.WriteLine("Please Enter your 4 digits PIN");
+if(!int.TryParse(Console.ReadLine(), out accountPin))
+{
+    Console.WriteLine("Transaction Canceld. Invalid PIN");
+    return;
+}
+
+Console.WriteLine("What you want to do today?");
+Console.WriteLine("1. Cash Withdrawal");
+Console.WriteLine("2. Cash Deposite"); 
+Console.WriteLine("3. Check Balance");
+Console.WriteLine("4. Reset ATM PIN");
+
+if (!int.TryParse(Console.ReadLine(), out operationType) || (operationType != 1 && operationType != 2 && operationType != 3 && operationType != 4))
+{
+    Console.WriteLine("Transaction Canceld. Please Select a valid option");
+    return;
+}
+
+if(operationType == 1)
+{
+    Console.WriteLine($"Available Balance: Rs {currentBalance}");
+    Console.WriteLine("How much you want to withdraw?");
+    if(!int.TryParse(Console.ReadLine(), out withdrawAmount))
+    {
+        Console.WriteLine("Transaction Canceld. Please enter valid amount");
+        return;
+    }
+
+    Console.WriteLine("Withdrawal Successful. Please collect your cash");
+
+    Console.WriteLine($"Available Balance: Rs {currentBalance - withdrawAmount}");
+
+} else if(operationType == 2)
+{
+
+} else if(operationType == 3)
+{
+
+} else if(operationType == 4)
+{
+
+}
